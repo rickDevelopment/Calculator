8dacbf2fe71c86d339dce4f2a1c9be36a1757926
Rick <alrick.chalar@gmail.com>
2019-01-03 21:25:36 -0500

    feat: add functions to handle math operations

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let runningTotal = 0;
 let buffer = '0';
 let previousOperator = null;
 //Get class from html document
-let screen = document.querySelector('.calc-screen');
+const screen = document.querySelector('.calc-screen');
 let buttonClicked = document.querySelector('.calc-buttons')
 
 //Function to handle clicked events
@@ -24,12 +24,12 @@ let buttonClicked = document.querySelector('.calc-buttons')
     screen.innerHTML = buffer;
   }
   //Function reset everthing to default when the clear button is pressed
-  function clearButton(){
-    buffer = '0';
-    runningTotal = 0;
-    previousOperator = null
-    rerender()
-  }
+  // function clearButton(){
+  //   buffer = '0';
+  //   runningTotal = 0;
+  //   previousOperator = null
+  //   rerender()
+  // }
 
   //addition function
 
@@ -56,26 +56,68 @@ let buttonClicked = document.querySelector('.calc-buttons')
   }
   // Function to run when a symbol is clicked 
   function handleSymbol(value){
-    if(previousOperator === null){
-      previousOperator = value;
-    }
+    // if(previousOperator === null){
+    //   previousOperator = value;
+    // }
     switch(value){
       case 'C':
-        clearButton()
+        buffer = '0';
+        runningTotal =0;
         break
       case '=':
-      runningTotal = parseInt(buffer)
-      console.log(runningTotal)
-          addition()
+        if(previousOperator === null){
+          //Need two numbers to math
+          return;
+        }
+        flushOperation(parseInt(buffer));
+          previousOperator = null;
+          buffer = ""+ runningTotal;
+          runningTotal = 0;
           break;
       case '←':
         if(buffer.length === 1){
           buffer = "0";
         }
         else{
-          buffer = buffer.slice(0, buffer.length-1)
+          buffer = buffer.substring(0, buffer.length-1);
         }
-        }
+        break;
+      case "+":
+      case "-":
+      case "✕":
+      case "÷":
+        handleMath(value);
+        break;
+    }
+  }
+  
+  function handleMath(value){
+    if(buffer === '0'){
+      return;
+    }
+
+    const intBuffer = parseInt(buffer);
+    if(runningTotal === 0){
+      runningTotal = intBuffer;
+    }
+    else{
+      flushOperation(intBuffer);
+    }
+    previousOperator = value;
+    
+    buffer = '0';
   }
 
-  
+  function flushOperation(intBuffer){
+    if(previousOperator === "+"){
+      runningTotal += intBuffer;
+
+    } else if(previousOperator === "-"){
+      runningTotal -= intBuffer;
+    } else if(previousOperator === "✕"){
+      runningTotal *= intBuffer;
+    
+  }else {
+    runningTotal /= intBuffer;
+  }
+}
